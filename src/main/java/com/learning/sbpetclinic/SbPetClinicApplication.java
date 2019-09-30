31b87960287b0d0d301e809653f109013ccbce78
Harikrishnan <hari.911@gmail.com>
2019-09-30 16:43:11 +0530

    Initial commit with files

---
+++ src/main/java/com/learning/sbpetclinic/SbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.learning.sbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPetClinicApplication.class, args);
+	}
+
+}
